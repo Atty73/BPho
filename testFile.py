21f22ee69d05820e84d250df5bd9000568db7347
Atty <atty.broadfoot@outlook.com>
2025-07-17 18:29:13 +0100

    Added tkinter to test file and image

--- testFile.py
+++ testFile.py
@@ -1,31 +1,40 @@
-from PIL import Image
+from PIL import Image, ImageTk
 import matplotlib.pyplot as plt
 import numpy as np
+import tkinter as tk
 
-img = Image.open("sheldon.png").convert('RGB')
-pixel_array = np.array(img)
+def invertImage(picture):
+    img = Image.open(picture).convert('RGB')
+    pixel_array = np.array(img)
 
-inverse_pixel_array = np.array(img)
+    height, width, _ = pixel_array.shape
 
-for i in range(len(pixel_array)):
-    print(inverse_pixel_array[-(i+1)])
-    print(pixel_array[0])
-    #inverse_pixel_array[:, [0, i]] = pixel_array[:, [i, 0]]
-    inverse_pixel_array = pixel_array[:, ::-1]
+    x_coords, y_coords = np.meshgrid(np.arange(width), np.arange(height))
 
-height, width, _ = pixel_array.shape
+    x_flat = x_coords.flatten()
+    y_flat = y_coords.flatten()
+    colors = pixel_array.reshape(-1, 3) / 255
 
-x_coords, y_coords = np.meshgrid(np.arange(width), np.arange(height))
+    neg_x_flat = -1*x_flat
 
-x_flat = x_coords.flatten()
-y_flat = y_coords.flatten()
-colors = pixel_array.reshape(-1, 3) / 255
+    plt.figure(figsize=(16, 8))
+    plt.scatter(x_flat, y_flat, c=colors, marker='s', s=1)
+    plt.scatter(neg_x_flat, y_flat, c=colors, marker='s', s=1)
+    plt.gca().invert_yaxis()
 
-neg_x_flat = -1*x_flat
+    newFilename = "matplotlib" + picture
+    plt.savefig(newFilename)
+    return newFilename
 
-plt.figure(figsize=(16, 8))
-plt.scatter(x_flat, y_flat, c=colors, marker='s', s=1)
-plt.scatter(neg_x_flat, y_flat, c=colors, marker='s', s=1)
-plt.gca().invert_yaxis()
-#plt.axis('off')
-plt.show()
+window = tk.Tk()
+window.title("Image")
+
+imageFilename = invertImage("sheldon.png")
+
+image = Image.open(imageFilename)
+image = ImageTk.PhotoImage(image)
+
+image_label = tk.Label(window, image=image)
+image_label.pack()
+
+window.mainloop()
